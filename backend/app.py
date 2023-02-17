e211d9fbb47b9b7c484e70a9298b4eeeaed201d6
LuanLuu <lluu3@mail.sfsu.edu>
2023-02-16 21:54:28 -0800

    backend added

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/api")
+def hello():
+    return {"res":"Hello"}
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "0.0.0.0", port = 3000)

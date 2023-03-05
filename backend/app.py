9cb631261bb36a664f3b5a2225ceb0ea4368a5a6
LuanLuu <lluu3@mail.sfsu.edu>
2023-03-05 14:31:15 -0800

    db connected

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import psycopg2
+import os 
 
 app = Flask(__name__)
 
 @app.route("/api")
 def hello():
+    conn = psycopg2.connect(database=os.environ['PG_DB'], 
+                            user=os.environ['PG_USER'],
+                            password=os.environ['PG_SECRET'], 
+                            host=os.environ['PG_HOST'], 
+                            port=os.environ['PG_PORT'])
     return {"res":"Hello"}
 
 if __name__ == "__main__":
-    app.run(debug = True, host = "0.0.0.0", port = 3000)
+    app.run(host = "0.0.0.0", port = 4000)

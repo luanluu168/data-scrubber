74782f74b9f1d531e948cb620a8dd417ae0e0006
LuanLuu <lluu3@mail.sfsu.edu>
2023-03-16 02:41:55 -0700

    deleteUser column added

--- frontend/src/BasicTable.js
+++ frontend/src/BasicTable.js
@@ -10,6 +10,7 @@ import { useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Stack from '@mui/material/Stack';
+import axios from 'axios';
 
 export default function BasicTable({message, rows, setRows}) {
     const TABLE_MIN_WIDTH = 450;
@@ -20,30 +21,37 @@ export default function BasicTable({message, rows, setRows}) {
                 return (<TableCell key={i} component="th" scope="row">{e}</TableCell>)
             } else if (i === 3) {
                 return (<TableCell key={i} component="th" scope="row">{e.slice(5, 17)}</TableCell>)
-            } else if (i === 6) {
-                return (<TableCell key={i} component="th" scope="row">
-                            <Stack direction="row" alignItems="center" spacing={1}>
-                                <IconButton aria-label="delete" size="small">
-                                    <DeleteIcon fontSize="inherit" />
-                                </IconButton>
-                            </Stack>
-                        </TableCell>);
             }
 
             return (<TableCell key={i} component="th" align="center">{e}</TableCell>)
         });
     }
 
+    async function deleteRow(value) {
+        console.log("deleteRow " + value);
+        const {data} = await axios.post('/api/deleteUser', {
+            unique_id: value
+        });
+        console.log(data);
+    }
+
     function createTableBody(rows) {
         return (
             <TableBody>
                 {
-                    rows.map(({element}) => (
+                    rows.map(({element, i}) => (
                         <TableRow
                             key={ element[0] }
                             sx={{ border: 1 }}
                         >
                             { getCols(element) }
+                            <TableCell key={i} component="th" scope="row">
+                                <Stack direction="row" alignItems="center" spacing={1}>
+                                    <IconButton aria-label="delete" size="small" value={element[0]} onClick={(event) => deleteRow(event.target.value)}>
+                                        <DeleteIcon fontSize="inherit" />
+                                    </IconButton>
+                                </Stack>
+                            </TableCell>
                         </TableRow>
                     ))
                 }

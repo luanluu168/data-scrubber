c06b33b9534d76564c77f3833a10c9d228489174
LuanLuu <lluu3@mail.sfsu.edu>
2023-04-22 13:43:34 -0700

    InsertFile icon and modified text added

--- frontend/src/InsertFiles.js
+++ frontend/src/InsertFiles.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 import axios from 'axios';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 export default function InsertFiles({setOpenModal, message, setMessage}) {
   function addUsers() {
@@ -12,6 +13,8 @@ export default function InsertFiles({setOpenModal, message, setMessage}) {
     <Button
     variant="contained"
     component="label"
+    sx={{ textTransform: 'capitalize' }} 
+    endIcon={<CloudUploadIcon/>}
     >
     Upload File
     <input

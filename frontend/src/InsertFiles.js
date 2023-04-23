b88b3b617581bbc2f14c9fd1d852e127d36d947c
LuanLuu <lluu3@mail.sfsu.edu>
2023-04-22 21:24:54 -0700

    Frontend sendFile updated

--- frontend/src/InsertFiles.js
+++ frontend/src/InsertFiles.js
@@ -4,22 +4,40 @@ import { useState } from 'react';
 import axios from 'axios';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
-export default function InsertFiles({setOpenModal, message, setMessage}) {
-  function addUsers() {
-    
+export default function InsertFiles() {
+  const [file, setFile] = useState("");
+
+  async function sendFile() {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const {data} = await axios.post('/api/sendFile', {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          data: formData
+      })
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
     <Button
-    variant="contained"
-    component="label"
-    sx={{ textTransform: 'capitalize' }} 
-    endIcon={<CloudUploadIcon/>}
+      variant="contained"
+      component="label"
+      sx={{ textTransform: 'capitalize' }} 
+      endIcon={<CloudUploadIcon/>}
     >
-    Upload File
+      Upload File
     <input
         type="file"
         hidden
+        onChange={(event)=> { 
+          setFile(event.target.files[0]) 
+          sendFile()
+        }}
     />
     </Button>
   );
